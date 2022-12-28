77e9e5d6b161856e8856147e8b26b3b53de35082
Yidaozhan Ya <yidaozhanyaqwq@gmail.com>
2022-12-28 21:33:51 +0800

    better outdated check

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Stellar Fork - 每个人的独立博客
 
-本主题是 xaoxuu/hexo-theme-stellar 的 fork，用于一刀斩的博客。
+本主题是 xaoxuu/hexo-theme-stellar 的 fork，用于[一刀斩的博客](https://blog.yidaozhan.top)。
 
 - 去 jsDelivr 化。
-- 样式做了修改，遵循 Fluent Design 设计风格。
+- 样式相比原版做了很大修改，遵循 Fluent Design 设计风格。
 
 自 1.17.2 起，本主题不再跟随 Stellar 主分支跟进版本，转而自己维护。
--- source/js/outdate-check.js
+++ source/js/outdate-check.js
@@ -1,40 +1,17 @@
 function judgeOutdated() {
   var postDate = new Date(document.getElementById("post-meta").firstElementChild.dateTime);
   var nowDate = new Date();
-  const year1 = postDate.getFullYear();
-  const year2 = nowDate.getFullYear();
-  const month1 = postDate.getMonth() + 1;
-  const month2 = nowDate.getMonth() + 1;
-  const day1 = postDate.getDate();
-  const day2 = nowDate.getDate();
-  m = 3; //几个月过时
-  if (year2 === year1) { // 判断两个日期 是否是同年
-    if (month2 - month1 > m) { // 相差是否 在m个月中
-      return true;
-    } else if (month2 - month1 === m) { // 如果正好为 m月 判断天数
-      if (day2 > day1) {
-        return true;
-      }
-    }
-  } else { // 不同年
-    if (year2 - year1 > 1) {
-      return true;
-    }
-    else if (year2 - year1 === 1) {
-      if (month2 > m || month1 + m - month2 < 12) {
-        return true;
-      }
-      else if (month1 + m - month2 === 12) { // 正好为m月 判断天数
-        if (day2 > day1) {
-          return true;
-        }
-      }
-    }
+  //判断这两个日期是否相差三个月以上
+  if (nowDate.getFullYear() - postDate.getFullYear() > 0) {
+    return true;
+  } else if (nowDate.getMonth() - postDate.getMonth() > 2) {
+    return true;
+  } else {
+    return false;
   }
-  return false;
 }
 
 
 document.addEventListener('DOMContentLoaded', function () {
-  if (judgeOutdated()) { document.getElementById("outdate").innerText = "，文章内容可能已经过时！" };
+  if (judgeOutdated()) { document.getElementById("outdate").innerText = "，文章内容可能已经过时" };
 });
